b392d43e3926d7d7215203cf91367e7d3220e448
William Eduardo Meza <williameduardomeza@gmail.com>
2022-12-03 22:05:03 -0500

    fix: use dotenv in config file

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const APP_PORT = process.env.APP_PORT || '8080';
 export const APP_HOST = process.env.APP_HOST || 'localhost';
 export const APP_HTTP_SCHEMA = process.env.APP_HTTP_SCHEMA || 'http';
